20398abeb3ea871d1777cbe4cc925fa5bc18d0dc
Matthew Ruffalo <matthew.ruffalo@case.edu>
2009-10-03 18:11:30 -0400

    Start to use three tables (probably errors in the code in this commit)

--- src/AlignmentMatrix.java
+++ src/AlignmentMatrix.java
@@ -4,31 +4,43 @@ public class AlignmentMatrix
 	private String top;
 	private String left;
 	private AlignmentElement[][] a;
+	private AlignmentElement[][] n;
+	private AlignmentElement[][] w;
 	private AlignmentScoringSystem scoringSystem;
 	public AlignmentMatrix(AlignmentScoringSystem scoringSystem_, String top_, String left_)
 	{
 		scoringSystem = scoringSystem_;
 		top = top_;
 		left = left_;
-		a = new AlignmentElement[top.length() + 1][];
-		for (int i = 0; i <= top.length(); i++)
+		a = new AlignmentElement[left.length() + 1][];
+		for (int i = 0; i <= left.length(); i++)
 		{
-			a[i] = new AlignmentElement[left.length() + 1];
+			a[i] = new AlignmentElement[top.length() + 1];
+		}
+		n = new AlignmentElement[left.length() + 1][];
+		for (int i = 0; i <= left.length(); i++)
+		{
+			n[i] = new AlignmentElement[top.length() + 1];
+		}
+		w = new AlignmentElement[left.length() + 1][];
+		for (int i = 0; i <= left.length(); i++)
+		{
+			w[i] = new AlignmentElement[top.length() + 1];
 		}
 		fillTable();
 	}
 	private void fillTable()
 	{
 		a[0][0] = new AlignmentElement(0, null);
-		a[1][0] = new AlignmentElement(scoringSystem.gapStart + scoringSystem.gapContinue, PointerDirection.LEFT);
+		a[1][0] = new AlignmentElement(scoringSystem.gapStart + scoringSystem.gapContinue, PointerDirection.UP);
 		for (int i = 2; i <= top.length(); i++)
 		{
-			a[i][0] = new AlignmentElement(a[i - 1][0].score + scoringSystem.gapContinue, PointerDirection.LEFT);
+			a[i][0] = new AlignmentElement(a[i - 1][0].score + scoringSystem.gapContinue, PointerDirection.UP);
 		}
-		a[0][1] = new AlignmentElement(scoringSystem.gapStart + scoringSystem.gapContinue, PointerDirection.UP);
+		a[0][1] = new AlignmentElement(scoringSystem.gapStart + scoringSystem.gapContinue, PointerDirection.LEFT);
 		for (int j = 2; j <= left.length(); j++)
 		{
-			a[0][j] = new AlignmentElement(a[0][j - 1].score + scoringSystem.gapContinue, PointerDirection.UP);
+			a[0][j] = n[0][j] = w[0][j]= new AlignmentElement(a[0][j - 1].score + scoringSystem.gapContinue, PointerDirection.LEFT);
 		}
 		for (int i = 0; i < top.length(); i++)
 		{
@@ -37,6 +49,8 @@ public class AlignmentMatrix
 				PointerDirection dir = null;
 				int score;
 				int characterScore = (top.charAt(i) == left.charAt(j)) ? scoringSystem.match : scoringSystem.mismatch;
+				// North
+				//int northGapStartScore = a[]
 			}
 		}
 	}
