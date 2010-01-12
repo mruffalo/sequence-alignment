1d9c9d006ab612c31a816caf95bc6107a41b3eaa
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-01-12 12:02:41 -0500

    Some building blocks for GUI

--- src/gui/SequenceAlignmentGui.java
+++ src/gui/SequenceAlignmentGui.java
@@ -1,7 +1,64 @@
 package gui;
 
+import javax.swing.*;
+import javax.swing.event.ListDataListener;
+import javax.swing.table.JTableHeader;
+import java.awt.Component;
+import java.awt.event.*;
+
 public class SequenceAlignmentGui
 {
+	private JFrame frame;
+	
+	private static class StringListModel extends AbstractListModel
+	{
+		private String string;
+		
+		public StringListModel(String string_)
+		{
+			string = string_;
+		}
+		
+		public void setString(String string_)
+		{
+			string = string_;
+		}
+		
+		@Override
+		public Object getElementAt(int index)
+		{
+			return string.substring(index, index);
+		}
+		
+		@Override
+		public int getSize()
+		{
+			return string.length();
+		}
+	}
+	
+	private static class RowHeaderRenderer extends JLabel implements ListCellRenderer
+	{
+		public RowHeaderRenderer(JTable table)
+		{
+			JTableHeader header = table.getTableHeader();
+			setOpaque(true);
+			setBorder(UIManager.getBorder("TableHeader.cellBorder"));
+			setHorizontalAlignment(CENTER);
+			setForeground(header.getForeground());
+			setBackground(header.getBackground());
+			setFont(header.getFont());
+		}
+		
+		@Override
+		public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
+			boolean cellHasFocus)
+		{
+			setText((value == null) ? "" : value.toString());
+			return this;
+		}
+	}
+	
 	/**
 	 * @param args
 	 */
